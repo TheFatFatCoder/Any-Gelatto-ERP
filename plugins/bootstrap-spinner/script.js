67b65fdaf7cc2db9d2cf645e8bf8afaeb746d536
Leonardo Theoson <leonardobahar@gmail.com>
2020-03-02 09:54:43 +0700

    Created landing page for loading

---
+++ plugins/bootstrap-spinner/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    // HIDE LOADING SPINNER WHEN PAGE IS LOADED [7000msec after the page is loaded]
+    $(window).on('load', function () {
+        setTimeout(function () {
+            $('.loader').hide(300);
+        }, 7000);
+    });
+
+
+
+    // EXECUTION BLOCK
+    $(window).on('load', function () {
+        var loadingCounter = setInterval(function () {
+            var count = parseInt($('.countdown').html());
+            if (count !== 0) {
+                $('.countdown').html(count - 1);
+            } else {
+                clearInterval();
+            }
+        }, 1000);
+    });
+    $('#reload').on('click', function (e) {
+        e.preventDefault();
+        location.reload();
+    });
+});
+
